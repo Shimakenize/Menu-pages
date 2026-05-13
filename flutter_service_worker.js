8069f3061de6e84d9bf5e40e844f1504c15ca2af
Shimakenize <smkn@mti.biglobe.ne.jp>
2026-05-13 15:56:31 +0900

    Add パン type, reduce categories to すべて/外食, fix LIFF swipe-close

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,10 +24,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.js.symbols": "0755b4fb399918388d71b59ad390b055",
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "9e5411635ae4b2a2231fb7112463d8fb",
-"index.html": "21637290fa0ec33acc46892da0c0affc",
-"/": "21637290fa0ec33acc46892da0c0affc",
-"main.dart.js": "8168bc696774b3d78a9bfb3cd12abb67",
+"flutter_bootstrap.js": "867b6fe376fa3acb71519b14bc04be42",
+"index.html": "75a781ec8878c4999961dc5cadbacc64",
+"/": "75a781ec8878c4999961dc5cadbacc64",
+"main.dart.js": "ca4a47a96555c57ecdfd10ec90f65c01",
 "version.json": "90b40ca2069f4a45472a59e0ac8fa190"};
 // The application shell files that are downloaded before a service worker can
 // start.
