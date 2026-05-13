2787eb334aade0be9e7494e1e95017c644f62785
Shimakenize <smkn@mti.biglobe.ne.jp>
2026-05-13 17:16:39 +0900

    fix: liff.init timeout, show userId on denied screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,10 +24,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.js.symbols": "0755b4fb399918388d71b59ad390b055",
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "c9b310a7d5329f3260726f2e11c16722",
+"flutter_bootstrap.js": "732d4f5a34157ec873f8e1a6e3544295",
 "index.html": "dca8e2a5a970f07929dfa6543cb8733e",
 "/": "dca8e2a5a970f07929dfa6543cb8733e",
-"main.dart.js": "285e1e4cd1667c1af3828ffd831a6c23",
+"main.dart.js": "3d19e58ed8ad04e025fd4954743d7b37",
 "version.json": "90b40ca2069f4a45472a59e0ac8fa190"};
 // The application shell files that are downloaded before a service worker can
 // start.
